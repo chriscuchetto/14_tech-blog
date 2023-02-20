46cad5464717579473058fe413ab340c3103451a
chriscuchetto <chriscuchetto@gmail.com>
2023-02-20 14:04:27 -0600

    move and modified files from activities folder

---
+++ controllers/home-routes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const Dish = require('../models/Dish');
+
+// route to get all dishes
+router.get('/', async (req, res) => {
+  const dishData = await Dish.findAll().catch((err) => { 
+      res.json(err);
+    });
+      const dishes = dishData.map((dish) => dish.get({ plain: true }));
+      res.render('all', { dishes });
+    });
+
+
+
+module.exports = router;
